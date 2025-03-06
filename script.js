9b00973952a3d2532eab2fd5f35cbe5f29b4da16
Letícia Lima <leticialimav@outlook.com.br>
2025-03-06 15:08:54 -0300

    Exercícios.

---
+++ script.js
@@ -0,0 +1,13 @@
+const nome = "JavaScript";
+console.log(nome);
+
+let total = 10;
+console.log(total);
+
+const nav = document.querySelector("nav");
+const produtos = document.querySelector(".produtos");
+const link = document.querySelector("#menu li a");
+
+console.log(nav);
+console.log(produtos);
+console.log(link);
