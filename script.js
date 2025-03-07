3d0fad2c527bcc751293c629e371b5d9b8dd5e42
Letícia Lima <leticialimav@outlook.com.br>
2025-03-07 11:25:36 -0300

    Compreensão de conceitos fundamentais JavaScript

--- script.js
+++ script.js
@@ -12,16 +12,62 @@
 // console.log(produtos);
 // console.log(link);
 
-const cursoJs = document.querySelector("#curso");
+// const cursoJs = document.querySelector("#curso");
 
-console.log(cursoJs.innerHTML);
-console.log(cursoJs.innerText);
-console.log(cursoJs.clientHeight);
-console.log(cursoJs.clientWidth);
+// console.log(cursoJs.innerHTML);
+// console.log(cursoJs.innerText);
+// console.log(cursoJs.clientHeight);
+// console.log(cursoJs.clientWidth);
 
-cursoJs.style.backgroundColor = "black";
-cursoJs.style.padding = "1rem";
-cursoJs.style.height = document.body.clientHeight / 2 + "px";
+// cursoJs.style.backgroundColor = "black";
+// cursoJs.style.padding = "1rem";
+// cursoJs.style.height = document.body.clientHeight / 2 + "px";
 
-cursoJs.classList.add("Ativo");
-cursoJs.setAttribute("aria-label", "Cursos");
+// cursoJs.classList.add("Ativo");
+// cursoJs.setAttribute("aria-label", "Cursos");
+
+// const texto = "Curso de JavaScript";
+// const adicional = "Novo";
+// const frase = texto + ": " + adicional;
+// console.log(frase);
+
+// let lotal = 0;
+
+// const compras = 30;
+// const imposto = 10;
+// total = compras + imposto;
+// console.log(total);
+
+// const dobro = total * 2;
+// const metade = total / 2;
+// const desconto = total - 20;
+
+// const strings = "20" + "20";
+// const numbers = 20 + 20;
+
+const nome = "O Senhor dos Anéis";
+const ano = 1954;
+const autor = "J. R. R. Tolkien";
+
+const nomeFilme = "O Senhor dos Anéis";
+const anoFilme = 2001;
+const diretorFilme = "Peter Jackson";
+
+const livro = {
+  nome: "O Senhor dos Anéis",
+  ano: 1954,
+  autor: "J. R. R. Tolkien",
+};
+
+const filme = {
+  nome: "O Senhor dos Anéis",
+  ano: 2001,
+  diretor: "Peter Jackson",
+};
+
+console.log(livro);
+console.log(livro.nome);
+console.log(livro.ano);
+
+console.log(filme);
+console.log(filme.diretor);
